a97784d767eb6363398f8bfa421ceb21e2fcfa37
Satish Gupta <satish.gupta@clik.ai>
2022-02-19 22:28:49 +0530

    Added meta tag on bio page

--- pages/bio.js
+++ pages/bio.js
@@ -94,18 +94,32 @@ const fields = [
 
 const BioPage = () => {
   return (
-    <div className="h-full sm:h-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-serif md:flex transition-all ease-in-out duration-600">
-      <div className="text-white bg-black text-center text-2xl sm:text-3xl md:text-2xl p-2 mb-3 md:mb-0 md:h-screen md:flex lg:w-1/3 flex-col transition-all ease-in-out duration-300 justify-center">
-        <img src="/img/profile-image-2.jpg" className="content-center  mx-auto transition-all ease-in-out duration-600 rounded-full ring-2 ring-indigo-300 mb-7 h-24 w-24 lg:h-32 lg:w-32" />
-        SATISH KUMAR GUPTA
+    <>
+      <Head>
+        <title>Satish Gupta</title>
+        <meta name="description" content="Satish Gupta, Highly skilled developer with experience of more than 4.5 years."></meta>
+        <meta property="og:title" content="Satish Gupta" />
+        <meta property="og:url" content="https://satishgupta.dev/" />
+        <meta property="og:description" content="Highly skilled developer with experience of more than 4.5 years." />
+        <meta property="og:image" content="https://satishgupta.dev/img/profile-image-reduced-quality.jpg" />
+        <meta property="og:type" content="profile" />
+        <meta property="og:profile:first_name" content="Satish" />
+        <meta property="og:profile:last_name" content="Gupta" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-16x16.png" />
+      </Head>
+      <div className="h-full sm:h-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-serif md:flex transition-all ease-in-out duration-600">
+        <div className="text-white bg-black text-center text-2xl sm:text-3xl md:text-2xl p-2 mb-3 md:mb-0 md:h-screen md:flex lg:w-1/3 flex-col transition-all ease-in-out duration-300 justify-center">
+          <img src="/img/profile-image-2.jpg" className="content-center  mx-auto transition-all ease-in-out duration-600 rounded-full ring-2 ring-indigo-300 mb-7 h-24 w-24 lg:h-32 lg:w-32" />
+          SATISH KUMAR GUPTA
+        </div>
+        <div className="text-xs sm:text-sm md:m-auto transition-all ease-in-out duration-600">
+          {
+            fields.map(({fieldName, value}, index) => <Field {...{fieldName, value, key: index}} />)
+          }
+        </div>
       </div>
-      {/* <div className="mt-1 bg-black text-xs text-black">_</div> */}
-      <div className="text-xs sm:text-sm md:m-auto transition-all ease-in-out duration-600">
-        {
-          fields.map(({fieldName, value}, index) => <Field {...{fieldName, value, key: index}} />)
-        }
-      </div>
-    </div>
+    </>
   );
 };
 
